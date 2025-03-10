6e966532bec40f0f388db670b3c2327e55c3dfb5
miaelfk <samiafakihfranca@gmail.com>
2025-03-10 14:03:18 -0300

    Adicionado CascadeType.REMOVE e validação de tema

--- blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -2,7 +2,10 @@ package com.generation.blogpessoal.controller;
 
 import java.util.List;  
 import java.util.Optional;
+
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;  
@@ -14,14 +17,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;  
 import org.springframework.web.bind.annotation.RestController;  
 import com.generation.blogpessoal.model.Postagem;  
-import com.generation.blogpessoal.repository.PostagemRepository;  
+import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 @RestController  
 @RequestMapping("/postagens")  
 public class PostagemController {  
 
     @Autowired  
-    private PostagemRepository postagemRepository;  
+    private PostagemRepository postagemRepository;
+    
+    @Autowired
+    private TemaRepository temaRepository;
 
     @GetMapping  
     public List<Postagem> getAll() {  
@@ -36,25 +43,35 @@ public class PostagemController {
         }
         return ResponseEntity.notFound().build();
     }
+    
     @GetMapping("/titulo")
     public List<Postagem> getByTitulo(@RequestParam("titulo") String titulo) {
         return postagemRepository.findAllByTituloContainingIgnoreCase(titulo);
     }
+    
     @PostMapping
-    public Postagem post(@RequestBody Postagem novaPostagem) {
-        return postagemRepository.save(novaPostagem);
+    public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+        if (temaRepository.existsById(postagem.getTema().getId()))
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(postagemRepository.save(postagem));
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
-    @PutMapping("/{id}")
-    public ResponseEntity<Postagem> put(@PathVariable Long id, @RequestBody Postagem postagemAtualizada) {
-        Optional<Postagem> postagemExiste = postagemRepository.findById(id);
-        if (postagemExiste.isPresent()) {
-            Postagem postagem = postagemExiste.get();
-            postagem.setTitulo(postagemAtualizada.getTitulo());
-            postagem.setTexto(postagemAtualizada.getTexto());
-            return ResponseEntity.ok(postagemRepository.save(postagem));
+    
+    @PutMapping
+    public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
+        if (postagemRepository.existsById(postagem.getId())) {
+            
+            if (temaRepository.existsById(postagem.getTema().getId()))
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(postagemRepository.save(postagem));
+                
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-        return ResponseEntity.notFound().build();
+        
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
+    
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         Optional<Postagem> postagem = postagemRepository.findById(id);
@@ -65,4 +82,3 @@ public class PostagemController {
         return ResponseEntity.notFound().build();
     }
 }
-
--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Tema.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -7,45 +7,47 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_temas")
 public class Tema {
 
-	 @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	    private String descricao;
+    private String descricao;
 
-	    @OneToMany(mappedBy = "tema")
-	    @JsonIgnoreProperties("tema")
-	    private List<Postagem> postagens;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("tema")
+    private List<Postagem> postagens;
 
-	    // Getters e Setters
-	    public Long getId() {
-	        return id;
-	    }
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
 
-	    public void setId(Long id) {
-	        this.id = id;
-	    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	    public String getDescricao() {
-	        return descricao;
-	    }
+    public String getDescricao() {
+        return descricao;
+    }
 
-	    public void setDescricao(String descricao) {
-	        this.descricao = descricao;
-	    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 
-	    public List<Postagem> getPostagens() {
-	        return postagens;
-	    }
+    public List<Postagem> getPostagens() {
+        return postagens;
+    }
 
-	    public void setPostagens(List<Postagem> postagens) {
-	        this.postagens = postagens;
-	    }
-	}
+    public void setPostagens(List<Postagem> postagens) {
+        this.postagens = postagens;
+    }
+}
 
